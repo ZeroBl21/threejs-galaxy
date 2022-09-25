373c6bea1d8aac7f0938dfadd2026bac15e73559
Darwin Perdomo <ZeroBl@hotmail.com>
2022-09-25 15:27:00 -0400

    feat: add back the galaxy color

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,5 @@
+varying vec3 vColor;
+
 void main() {
   /* float strength = distance(gl_PointCoord, vec2(0.5)); */
   /* strength = step(0.5, strength); */
@@ -11,6 +13,8 @@ void main() {
   strength = 1.0 - strength;
   strength = pow(strength, 5.0);
 
-  gl_FragColor = vec4(vec3(strength), 1.0);
+  // Final Color
+  vec3 color = mix(vec3(0.0), vColor, strength);
 
+  gl_FragColor = vec4(color, 1.0);
 }
