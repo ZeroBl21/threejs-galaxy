7d61a1533b5e9a069ae7148c6a7d3a1233c417fc
Darwin Perdomo <ZeroBl@hotmail.com>
2022-09-25 15:22:12 -0400

    feat: add star custom texture

--- src/shaders/galaxy/vertex.glsl
+++ src/shaders/galaxy/vertex.glsl
@@ -6,11 +6,12 @@ void main() {
   // Position
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  vec4 viewPositon = viewMatrix * modelPosition;
-  vec4 projectedPosition = projectionMatrix * viewPositon;
-
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+  
   gl_Position = projectedPosition;
 
   // Size
   gl_PointSize = uSize * aScale;
+  gl_PointSize *=  (1.0 / - viewPosition.z);
 }
